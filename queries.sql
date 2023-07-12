3c517ed70ea551957c22ca50599eb4b6ebd1e9a9
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-07-12 10:21:47 -0800

    Add more queries

--- queries.sql
+++ queries.sql
@@ -9,3 +9,41 @@ SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE name != 'Gabumon';
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+ROLLBACK;
+SELECT * from animals;
+BEGIN;
+UPDATE animals
+SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals
+SET species = 'pokemon' WHERE species IS null;
+COMMIT;
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * from animals;
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT sv1;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK to sv1;
+UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT SUM(escape_attempts) FROM animals WHERE neutered IS true;
+SELECT CASE
+  WHEN (SELECT SUM(escape_attempts) FROM animals WHERE neutered IS true) >
+       (SELECT SUM(escape_attempts) FROM animals WHERE neutered IS false)
+    THEN 'Neutered Animals'
+  ELSE 'Not Neutered Animals'
+END AS result;
+SELECT MAX(weight_kg), MIN(weight_kg) FROM animals;
+SELECT
+  AVG(CASE WHEN species = 'pokemon' THEN escape_attempts END) AS pokemon,
+  AVG(CASE WHEN species = 'digimon' THEN escape_attempts END) AS digimon
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
