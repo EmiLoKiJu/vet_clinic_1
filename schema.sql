e8d55ee9030f05e13ad9a10e3fc51c9ea782246a
EmiLoKiJu <gabrielrozas1996@gmail.com>
2023-07-14 16:22:40 -0800

    Add more queries

--- schema.sql
+++ schema.sql
@@ -29,3 +29,23 @@ ADD COLUMN id SERIAL PRIMARY KEY;
 ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id int REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id int REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id serial PRIMARY KEY,
+    name varchar(255),
+    age int,
+	date_of_graduatio date
+);
+
+CREATE TABLE specializations (
+	vet_id int REFERENCES vets(id),
+	species_id int REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+	vet_id int REFERENCES vets(id),
+	animal_id int REFERENCES animals(id)
+);
+
+ALTER TABLE vets RENAME COLUMN date_of_graduatio TO date_of_graduation;
+ALTER TABLE visits ADD COLUMN date_of_visit date;
